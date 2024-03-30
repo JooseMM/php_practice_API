98b7ab8fd5f222a5b6ceda225e501b651dba0011
Jose <josexmoreno1998@gmail.com>
2024-03-29 21:49:56 -0300

    remove unneccesary null checker

--- server.php
+++ server.php
@@ -3,7 +3,7 @@ include "./routes.php";
 
 $current_url = $_SERVER['REQUEST_URI'];
 $method_type = $_SERVER['REQUEST_METHOD'];
-$url_query = $_SERVER['QUERY_STRING'] ?? false;
+$url_query = $_SERVER['QUERY_STRING']; 
 
 switch($method_type)
 {
