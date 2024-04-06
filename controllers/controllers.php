a00b8c80c014dd98ddc0f1f2f114dc7f706adebb
Jose <josexmoreno1998@gmail.com>
2024-04-06 14:34:31 -0300

    format database class

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -9,50 +9,53 @@ class GetController {
 		Response::send_json($response["successful"], $response["query_result"], $response["message"]);
 	}
 
-	public static function get($raw_query) {
-		$valid_query = Request::parse_query($raw_query);
-		if(!$valid_query) 
-			return Response::send_json(false, NULL, "Bad string query"); 
-		if(!array_key_exists("id", $valid_query)) 
+	public static function get($raw) {
+		if(!DataValidation::valid_string_query($raw))
+			return Response::send_json(false, NULL, "Invalid query string provided");   
+		$arr = Request::to_array($raw);
+		if(!array_key_exists("id", $arr)) {
+			return Response::send_json(false, NULL, "No id provided");   
+		} else if(!Datavalidation::is_valid_id($arr["id"])) {
 			return Response::send_json(false, NULL, "Invalid id provided");   
-		$response = MyDatabase::get_one($valid_query['id']);
-		return Response::send_json($response["successful"], $response["query_result"], $response["message"]); 
+		}
+		$res = MyDatabase::get_one($arr['id']);
+		return Response::send_json($res["successful"], $res["query_result"], $res["message"]); 
 	}
 }
 
 class PostController {
-	public static function add($raw_input) {
-		if(!DataValidation::is_valid_name($raw_input)) 
+	public static function add($raw) {
+		if(!DataValidation::is_valid_name($raw["name"])) 
 			return Response::send_json(false, NULL, "Invalid name provided"); 
-		if(!DataValidation::is_valid_age($raw_input)) 
+		if(!DataValidation::is_valid_age($raw["age"])) 
 			return Response::send_json(false, NULL, "Invalid age provided"); 
-		$is_successful = MyDatabase::add_one($raw_input);
-		return Response::send_json($is_successful["successful"], $is_successful["data"] , $is_successful["message"]);
+		$res = MyDatabase::add_one($raw);
+		return Response::send_json($res["successful"], $res["data"] , $res["message"]);
 
 	}
-	public static function remove($array_input) {
-		if(!datavalidation::is_valid_id($array_input["id"])) 
+	public static function remove($arr) {
+		if(!datavalidation::is_valid_id($arr["id"])) 
 			return response::send_json(false, null, "invalid id provided"); 
-		$response = mydatabase::remove_one($array_input["id"]);
-		return response::send_json($response["successful"], $response["query_result"], $response["message"]);
+		$res = mydatabase::remove_one($arr["id"]);
+		return response::send_json($res["successful"], $res["query_result"], $res["message"]);
 	}
-  	public static function update_one($raw_data) {
-		foreach($raw_data as $val) {
+  	public static function update_one($arr) {
+		foreach($arr as $val) {
 			if($val == NULL || $val === " " || $val === "\n" || $val === "\t")
 				return Response::send_json(false, NULL, "Incomplete data provided"); 
 		}
 
-		if(!DataValidation::is_valid_id($raw_data["id"]))
+		if(!DataValidation::is_valid_id($arr["id"]))
 			return Response::send_json(false, NULL, "Invalid ID provided");
 
-		if(!DataValidation::is_valid_name($raw_data["name"])) 
+		if(!DataValidation::is_valid_name($arr["name"])) 
 			return Response::send_json(false, NULL, "Invalid name provided");
 
-		if(!DataValidation::is_valid_age($raw_data["age"])) 
+		if(!DataValidation::is_valid_age($arr["age"])) 
 			return Response::send_json(false, NULL, "Invalid age provided");
 
-		$result = MyDatabase::update_one($raw_data);
-		return Response::send_json($result["successful"], $result["data"], $result["message"]);
+		$res = MyDatabase::update_one($arr);
+		return Response::send_json($res["successful"], $res["data"], $res["message"]);
 
 	}
 }
