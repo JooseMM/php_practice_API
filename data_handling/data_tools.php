97fe247cb5af1761149b61aeacee09d43c117ebb
Jose <josexmoreno1998@gmail.com>
2024-04-06 14:34:43 -0300

    format database class

--- data_handling/data_tools.php
+++ data_handling/data_tools.php
@@ -1,27 +1,17 @@
 <?php
 
 class Request {
-	public static function parse_query($query_string) {
-    		if(!DataValidation::validate_string_query($query_string))
-      			return NULL;
+	public static function to_array($qrs) {
 
-    	$multiple_queries = str_contains($query_string, '&');
-    	$raw_array = $multiple_queries ? explode('&', $query_string) : $query_string; //separete values and keys from each other
-    	$final_array = [];
+		$rgx = '/(\w+)=(\w+)(?:&|$)/';
+		preg_match_all($rgx, $qrs, $mat, PREG_SET_ORDER);
+		$res = array();
+		foreach ($mat as $val) {
+			$res[$val[1]] = $val[2];
+		}
+		return $res;
 
-    	if($multiple_queries) {
-      		for($i = 0; $i < count($raw_array); $i++) {
-			$raw_array[$i] = explode('=', $raw_array[$i]); //separete values and keys 
-			$final_array[$raw_array[$i][0]] = $raw_array[$i][0] == 'age' ? (int)$raw_array[$i][1] : $raw_array[$i][1]; // normalize keys and values into a new associative array
-      		}
-      		return $final_array;
-    	}
-
-	$raw_array = explode('=', $raw_array); 
-	$final_array[$raw_array[0]] = $raw_array[0] == 'age' ? (int)$raw_array[1] : $raw_array[1]; // normalize keys and values into a new associative array
-    
-    return $final_array;
-  }
+  	}
 	public static function get_specific_params($array) {
 		switch(array_key_first($array)) {
 		case "name": 
@@ -56,33 +46,33 @@ class Response {
 
 class DataValidation {
 
-	public static function validate_string_query($raw_string_query) {
-    		$only_string_queries = "/^[a-zA-Z]+=[a-zA-Z0-9]+$/";
-		if(!preg_match($only_string_queries, $raw_string_query)) 
+	public static function valid_string_query($str) {
+    		$only_string_queries = "/\w+=(?:\w+|\d+)(?:&\w+=(?:\w+|\d+))*/";
+		if(!preg_match($only_string_queries, $str)) 
 			return false; 
 		else 
     			return true;
   }
 
-	public static function is_valid_name($val) {
+	public static function is_valid_name($str) {
 		$only_letters = "/^[a-zA-Z]+$/"; 
-		if(!preg_match($only_letters, $val)) 
+		if(!preg_match($only_letters, $str)) 
 			return false; 
 		else 
 			return true;
 
   	}
-  	public static function is_valid_age($val) {
+  	public static function is_valid_age($str) {
 		$only_age = "/^(?:[1-9]|[1-9][0-9]|1[0-4][0-9]|150)$/";
-		if(!preg_match($only_age, $val)) 
+		if(!preg_match($only_age, $str)) 
 			return false; 
 		else 
 			return true;
 
   	}
-	public static function is_valid_id($val) {
+	public static function is_valid_id($str) {
 		$only_letters_numbers = "/^[a-zA-Z0-9]+$/";
-		if(!preg_match($only_letters_numbers, $val)) 
+		if(!preg_match($only_letters_numbers, $str)) 
 			return false;
 		else
 			return true;
--- database/database_class.php
+++ database/database_class.php
@@ -1,104 +1,139 @@
 <?php
 
-class MyDatabase 
-{
-  //property declaration
-  public static $db_path = './database/example.db';
+class MyDatabase {
+	public static $db_path = './database/example.db';
 
-  public static function get_one($person_id)
-  {
-    try 
-    {
-      $pdo = new PDO("sqlite:" . self::$db_path);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $statement = $pdo->prepare("SELECT * FROM example WHERE id = :id ");
-      $statement->bindValue(":id", $person_id, PDO::PARAM_STR);
-      $statement->execute();
-      $query_result = [];
-      while($row = $statement->fetch(PDO::FETCH_ASSOC)) { $query_result[] = $row; }
-      $successful = count($query_result) > 0 ? true : false;
-      return [ "successful" => $successful, "query_result" => $query_result, "message" => $successful ? NULL : "Unable to retrive the data" ];
-    }
-    catch(PDOException $e) { return [ "successful" => false, "query_result" => NULL, "message" => "Connection failed: " . $e->getMessage() ]; }
-  }
-
-  public static function get_all()
-  { 
-    try {
-      $pdo = new PDO("sqlite:" . self::$db_path);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $successful = $pdo->query('SELECT * FROM example');
-      $pdo = NULL;
-      $query_result = [];
-      while($row = $successful->fetch(PDO::FETCH_ASSOC)) { $query_result[] = $row; }
-      return [ "successful" => $successful ? true : false , "query_result" => $query_result, "message" => NULL ];
-    }
-    catch(PDOException $e) { return [ "successful" => false, "query_result" => NULL, "message" => "Connection failed: " . $e->getMessage() ]; }
-
-  }
-
-  public static function add_one($new_person)
-  {
-    $person_name = $new_person["name"];
-    $person_age = $new_person["age"];
-    $person_id = uniqid();
-    try 
-    {
-      $pdo = new PDO("sqlite:" . self::$db_path);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $statement = $pdo->prepare("INSERT INTO example VALUES (:id, :name, :age)");
-      $statement->bindValue(":id", $person_id, PDO::PARAM_STR);
-      $statement->bindValue(":name", $person_name, PDO::PARAM_STR);
-      $statement->bindValue(":age", $person_age, PDO::PARAM_INT);
-      $successful = $statement->execute();
-      $pdo = NULL;
-      return [ "successful" => $successful, "query_result" => NULL, "message" => "Data added successfully" ];
-    }
-    catch(PDOException $e) { return [ "successful" => false, "query_result" => NULL, "message" => "Connection failed: " . $e->getMessage() ]; }
-  }
-
-  public static function remove_one($target_id)
-  {
-    try 
-    {
-      if( !(self::get_one($target_id))["successful"] ) { return [ "successful" => false, "query_result" => NULL, "message" => "No match found" ]; }
-
-      $pdo = new PDO("sqlite:" . self::$db_path);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $statement = $pdo->prepare("DELETE FROM example WHERE id = :id");
-      $statement->bindValue(":id", $target_id, PDO::PARAM_STR);
-      $statement->execute();
-      $pdo = NULL;
-      return [ "successful" => true, "query_result" => NULL, "message" => "Data removed successfully" ];
-    }
-    catch(PDOException $e) { return [ "successful" => false, "query_result" => NULL, "message" => "Connection failed: " . $e->getMessage() ]; }
-  }
-
-  public static function update_one($newp) {
-    try {
-	$pdo = new PDO("sqlite:" . self::$db_path);
-     	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //setting attributes to throw exeptions
-     	$stm = $pdo->prepare(
-     	        "UPDATE example SET name = :new_name, age = :new_age WHERE id = :id"
-     	);
-     	$stm->bindValue(":new_name", $newp["name"], PDO::PARAM_STR);
-     	$stm->bindValue(":new_age", $newp["age"], PDO::PARAM_INT);
-     	$stm->bindValue(":id", $newp["id"], PDO::PARAM_STR);
-     	$pdo = NULL;
-     	$stm->execute();
-     	return [
-		"successful" => true, 
-		"query_result" => NULL,
-		"message" => "Data updated successfully"
-       	];
-	} catch(PDOException $e) { 
-		return [ 
-		"successful" => false,
-		"query_result" => NULL,
-		"message" => "Connection failed: " . $e->getMessage()
-	       	]; 
+	public static function get_one($person_id) {
+		try {
+			$pdo = new PDO("sqlite:" . self::$db_path);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$statement = $pdo->prepare("SELECT * FROM example WHERE id = :id ");
+			$statement->bindValue(":id", $person_id, PDO::PARAM_STR);
+			$statement->execute();
+			$query_result = [];
+			while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+				$query_result[] = $row; 
+			}
+			$successful = count($query_result) > 0 ? true : false;
+			return [
+				"successful" => $successful,
+				"query_result" => $query_result,
+				"message" => $successful ? NULL : "Unable to retrive the data" 
+			];
+		} catch(PDOException $e) {
+			return [
+				"successful" => false,
+				"query_result" => NULL,
+				"message" => "Connection failed: " . $e->getMessage() 
+			];
+	       	}
 	}
-  }
+
+	public static function get_all() { 
+		try {
+			$pdo = new PDO("sqlite:" . self::$db_path);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$successful = $pdo->query('SELECT * FROM example');
+			$pdo = NULL;
+			$query_result = [];
+			while($row = $successful->fetch(PDO::FETCH_ASSOC)) {
+				$query_result[] = $row;
+		       	}
+			return [
+				"successful" => $successful ? true : false ,
+				"query_result" => $query_result,
+				"message" => NULL 
+			];
+		} catch(PDOException $e) {
+			return [
+				"successful" => false,
+				"query_result" => NULL,
+				"message" => "Connection failed: " . $e->getMessage() 
+			]; 
+		}
+
+  	}
+
+	public static function add_one($new_person) {
+		$person_name = $new_person["name"];
+		$person_age = $new_person["age"];
+		$person_id = uniqid();
+		try {
+			$pdo = new PDO("sqlite:" . self::$db_path);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$statement = $pdo->prepare("INSERT INTO example VALUES (:id, :name, :age)");
+			$statement->bindValue(":id", $person_id, PDO::PARAM_STR);
+			$statement->bindValue(":name", $person_name, PDO::PARAM_STR);
+			$statement->bindValue(":age", $person_age, PDO::PARAM_INT);
+			$successful = $statement->execute();
+			$pdo = NULL;
+			return [
+				"successful" => $successful,
+				"query_result" => NULL,
+				"message" => "Data added successfully" 
+			];
+		} catch(PDOException $e) {
+			return [
+				"successful" => false,
+				"query_result" => NULL,
+				"message" => "Connection failed: " . $e->getMessage() 
+			]; 
+		}
+  	}
+
+	public static function remove_one($target_id) {
+		try {
+			if(!(self::get_one($target_id))["successful"]) {
+				return [
+					"successful" => false,
+					"query_result" => NULL,
+					"message" => "No match found" 
+				]; 
+			}
+
+			$pdo = new PDO("sqlite:" . self::$db_path);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$statement = $pdo->prepare("DELETE FROM example WHERE id = :id");
+			$statement->bindValue(":id", $target_id, PDO::PARAM_STR);
+			$statement->execute();
+			$pdo = NULL;
+			return [
+				"successful" => true,
+				"query_result" => NULL,
+				"message" => "Data removed successfully" 
+			];
+		} catch(PDOException $e) {
+			return [
+				"successful" => false,
+				"query_result" => NULL,
+				"message" => "Connection failed: " . $e->getMessage() 
+			]; 
+		}
+  	}
+
+	public static function update_one($newp) {
+		try {
+			$pdo = new PDO("sqlite:" . self::$db_path);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //setting attributes to throw exeptions
+			$stm = $pdo->prepare("UPDATE example SET name = :new_name, age = :new_age WHERE id = :id");
+			$stm->bindValue(":new_name", $newp["name"], PDO::PARAM_STR);
+			$stm->bindValue(":new_age", $newp["age"], PDO::PARAM_INT);
+			$stm->bindValue(":id", $newp["id"], PDO::PARAM_STR);
+			$pdo = NULL;
+			$stm->execute();
+			return [
+				"successful" => true, 
+				"query_result" => NULL,
+				"message" => "Data updated successfully"
+			];
+		} catch(PDOException $e) { 
+			return [ 
+			"successful" => false,
+			"query_result" => NULL,
+			"message" => "Connection failed: " . $e->getMessage()
+			]; 
+		}
+	}
 
 }
 
